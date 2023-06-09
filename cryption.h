514f8352578e9ee48996aa906a7e05d073cfed6a
ARV34 <44635179+ARV34@users.noreply.github.com>
2023-06-10 02:48:57 +0700

    Update cryption.h

--- cryption.h
+++ cryption.h
@@ -9,11 +9,13 @@
 
 #include <stdint.h>
 
+#define CRYPT_KEY_LEN 32
+
 void Crypt_GenerateKey(int key_size, uint8_t *key);
 void Crypt_Encrypt(const uint8_t *in, int size, const uint8_t *key, 
-    int len_key, uint8_t *out);
+	int len_key, uint8_t *out);
 void Crypt_Decrypt(const uint8_t *in, int size, const uint8_t *key, 
-    int len_key, uint8_t *out);
+	int len_key, uint8_t *out);
 void Crypt_EncryptFixedKey(const uint8_t *in, int size, uint8_t *out);
 void Crypt_DecryptFixedKey(const uint8_t *in, int size, uint8_t *out);
 
